6ce82e55666acba0a931ecf9bff0380155a6c1c1
yulei <zc_1994@163.com>
2023-03-27 18:15:57 +0200

    change momentum error to relative error

--- utility/Trainer.py
+++ utility/Trainer.py
@@ -204,7 +204,7 @@ class Trainer:
             sum_tn += ((batch_pred == 0) & (truth_label == 0)).sum().item()
             sum_fn += ((batch_pred == 0) & (truth_label == 1)).sum().item()
             # Count the difference between truth p and predicted p
-            if cfg['momentum_predict']: diff_list.append((p_pred - p_truth))
+            if cfg['momentum_predict']: diff_list.append((p_pred - p_truth) / p_truth)
             self.logger.debug(' valid batch %i, loss %.4f', i, batch_loss)
 
         # Summarize the validation epoch
