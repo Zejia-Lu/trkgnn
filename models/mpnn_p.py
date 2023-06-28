3f114b743494dcddad3b31e87f8c7cffbd08bf50
yulei <zc_1994@163.com>
2023-06-28 14:15:20 +0200

    remove checkpoint

--- models/mpnn_p.py
+++ models/mpnn_p.py
@@ -27,9 +27,11 @@ class GNN(nn.Module):
     - an edge classifier
     """
 
-    def __init__(self, input_dim, hidden_dim,
-                 n_encoder_layers=2, n_edge_layers=4, n_node_layers=4,
-                 n_graph_iters=1, layer_norm=True):
+    def __init__(
+            self, input_dim, hidden_dim,
+            n_encoder_layers=2, n_edge_layers=4, n_node_layers=4,
+            n_graph_iters=1, layer_norm=True
+    ):
         super(GNN, self).__init__()
         self.n_graph_iters = n_graph_iters
 
@@ -37,21 +39,31 @@ class GNN(nn.Module):
         self.node_encoder = make_mlp(input_dim, [hidden_dim] * n_encoder_layers)
 
         # The edge network computes new edge features from connected nodes
-        self.edge_network = make_mlp(2 * hidden_dim, [hidden_dim] * n_edge_layers,
-                                     layer_norm=layer_norm)
+        self.edge_network = make_mlp(
+            input_size=2 * hidden_dim,
+            sizes=[hidden_dim] * n_edge_layers,
+            layer_norm=layer_norm
+        )
 
         # The node network computes new node features
-        self.node_network = make_mlp(2 * hidden_dim, [hidden_dim] * n_node_layers,
-                                     layer_norm=layer_norm)
+        self.node_network = make_mlp(
+            input_size=2 * hidden_dim,
+            sizes=[hidden_dim] * n_node_layers,
+            layer_norm=layer_norm
+        )
 
         # The edge classifier computes final edge scores
         self.edge_classifier = make_mlp(
-            2 * hidden_dim, [hidden_dim, 1], output_activation=None
+            input_size=2 * hidden_dim,
+            sizes=[hidden_dim, 1],
+            output_activation=None
         )
 
         # The momentum change regressor computes the momentum change for each edge
         self.momentum_change_regressor = make_mlp(
-            2 * hidden_dim, [hidden_dim, 1], output_activation=None
+            input_size=2 * hidden_dim,
+            sizes=[hidden_dim, 1],
+            output_activation=None
         )
 
     def forward(self, data, verbose=False):
@@ -69,41 +81,35 @@ class GNN(nn.Module):
             print_gpu_info(prefix="node_encoder")
 
         # We can move the checkpointing to this level
-        def computation(x):
-            for i in range(self.n_graph_iters):
-                # Previous hidden state
-                x0 = x
+        for i in range(self.n_graph_iters):
+            # Previous hidden state
+            x0 = x
 
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 1".format(i))
-                # Compute new edge features
-                edge_inputs = torch.cat([x[send_idx], x[recv_idx]], dim=1)
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 2".format(i))
-                e = self.edge_network(edge_inputs)
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 3".format(i))
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 1".format(i))
+            # Compute new edge features
+            edge_inputs = torch.cat([x[send_idx], x[recv_idx]], dim=1)
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 2".format(i))
+            e = self.edge_network(edge_inputs)
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 3".format(i))
 
-                # Sum edge features coming into each node
-                aggr_messages = scatter_add(e, recv_idx, dim=0, dim_size=x.shape[0])
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 4".format(i))
-                # Compute new node features
-                node_inputs = torch.cat([x, aggr_messages], dim=1)
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 5".format(i))
-                x = self.node_network(node_inputs)
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 6".format(i))
-                # Residual connection
-                x = x + x0
-                if verbose:
-                    print_gpu_info(prefix="graph iteration {}: 7".format(i))
-
-            return x
-
-        # Call computation with checkpoint
-        x = checkpoint(computation, x)
+            # Sum edge features coming into each node
+            aggr_messages = scatter_add(e, recv_idx, dim=0, dim_size=x.shape[0])
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 4".format(i))
+            # Compute new node features
+            node_inputs = torch.cat([x, aggr_messages], dim=1)
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 5".format(i))
+            x = self.node_network(node_inputs)
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 6".format(i))
+            # Residual connection
+            x = x + x0
+            if verbose:
+                print_gpu_info(prefix="graph iteration {}: 7".format(i))
 
         # Compute final edge scores; use original edge directions only
         start_idx, end_idx = data.edge_index
